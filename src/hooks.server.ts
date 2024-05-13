6b3e7fef1b3f9d4219a783b71aecc7f31f3a1a72
Gary Bortosky <garybort@gmail.com>
2024-05-13 15:27:28 -0500

    add environment variable

---
+++ src/hooks.server.ts
@@ -0,0 +1,10 @@
+import { dev } from '$app/environment';
+import { PUBLIC_DB_HOST } from '$env/static/public';
+import type { Handle } from '@sveltejs/kit';
+
+export const handle = (async ({ event, resolve }) => {
+	console.log('here i am');
+	console.log(PUBLIC_DB_HOST, dev);
+
+  return await resolve(event);
+}) satisfies Handle;
