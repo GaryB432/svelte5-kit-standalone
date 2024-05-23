feea40237530effb9d291551a9476660111cc7e7
Gary Bortosky <garybort@gmail.com>
2024-05-23 10:24:06 -0500

    add snippet

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,6 @@ import { PUBLIC_DB_HOST } from '$env/static/public';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle = (async ({ event, resolve }) => {
-	console.log('here i am');
 	console.log(PUBLIC_DB_HOST, dev);
 
 	return await resolve(event);
